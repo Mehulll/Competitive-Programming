0137fa70d200f51c90085cd927ad6d0710a7e9b0
developer19m <42600672+Mehulll@users.noreply.github.com>
2020-12-25 11:40:08 +0530

    Create binary_indexed_tree.cpp

---
+++ binary_indexed_tree.cpp
@@ -0,0 +1,24 @@
+template<typename T>
+struct BIT {
+  int n;
+  vector<T> d;
+  BIT(int n=0):n(n),d(n+1) {}
+  void add(int i, T x=1) {
+    for (i++; i <= n; i += i&-i) {
+      d[i] += x;
+    }
+  }
+  void replace(int i , T v){
+    add(i,v-sum(i,i+1));
+  }
+  T sum(int i) {
+    T x = 0;
+    for (i++; i; i -= i&-i) {
+      x += d[i];
+    }
+    return x;
+  }
+  T sum(int l, int r) {
+    return sum(r-1) - sum(l-1);
+  }
+};
