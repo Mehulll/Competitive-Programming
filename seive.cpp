567f4ac53757d9468a7524fddf9fdc67d42f9fc5
developer19m <42600672+Mehulll@users.noreply.github.com>
2021-01-07 09:28:17 +0530

    Create seive.cpp

---
+++ seive.cpp
@@ -0,0 +1,26 @@
+vector<int> smallest_factor;
+vector<bool> prime;
+vector<int> primes;
+ 
+void sieve(int maximum) {
+    maximum = max(maximum, 1);
+    smallest_factor.assign(maximum + 1, 0);
+    prime.assign(maximum + 1, true);
+    prime[0] = prime[1] = false;
+    primes = {};
+ 
+    for (int p = 2; p <= maximum; p++)
+        if (prime[p]) {
+            smallest_factor[p] = p;
+            primes.push_back(p);
+ 
+            for (int64_t i = int64_t(p) * p; i <= maximum; i += p)
+                if (prime[i]) {
+                    prime[i] = false;
+                    smallest_factor[i] = p;
+                }
+        }
+}
+
+const A_MAX = 1e6+5;
+seive(A_MAX);
