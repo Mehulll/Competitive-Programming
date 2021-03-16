413817b69de38100c510bfc6fe97b6db5a516de3
developer19m <42600672+Mehulll@users.noreply.github.com>
2021-03-16 16:46:49 +0530

    Update bitmasks.cpp

--- bitmasks.cpp
+++ bitmasks.cpp
@@ -8,3 +8,33 @@ for(int i = 0 ; i < (1 << n) ; i++){
     }
   }
 }
+
+struct Solver {
+ void solve(){
+   int n; cin >> n;
+    int ax = 1023 , in = 0;
+    for(int i = 0 ; i < n ; i++){
+      char x; int num;
+      cin >> x >> num;
+      if(x == '|'){
+        ax |= num;
+        in |= num;
+      }
+      else if(x == '&'){
+        ax &= num;
+        in &= num;
+      }
+      else{
+        ax ^= num;
+        in ^= num;
+      }
+    }
+    int xornum = ~ax;
+    int ornum = ~(ax ^ in);
+    xornum &= 1023;
+    ornum &= 1023;
+    cout<<2<<"\n";
+    cout<<"| "<<ornum<<"\n";
+    cout<<"^ "<<xornum<<"\n";
+  }
+};
