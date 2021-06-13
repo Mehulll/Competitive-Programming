cc2e5665f1a88932d6f566d5ec31e9bf00919fe5
developer19m <42600672+Mehulll@users.noreply.github.com>
2021-06-13 14:32:14 +0530

    Update Cycle.cpp

--- Graph/Cycle.cpp
+++ Graph/Cycle.cpp
@@ -1,15 +1,15 @@
 bool vis2[nax];
 
 bool cycle(int node , int par){
-	vis2[node] = 1;
-	for(int child : edges[node]){
-		if(vis2[child] == 0){
-			if(cycle(child,node) == true)
-				return true;
-		}
-		else
-			if(child != par)
-				return true;
-	}
-	return false;
+    	vis2[node] = 1;
+    	for(int child : edges[node]){
+    		if(!vis2[child]){
+    			if(cycle(child,node))
+    				return true;
+    		}
+    		else
+    			if(child != par || child == node)
+    				return true;
+    	}
+    	return false;
 }
